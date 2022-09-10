418affe431690811ffdbc5929adea42c3bd6fa0d
Tanay Saxena <tanay.saxena002@gmail.com>
2022-09-11 01:29:43 +0530

    Default Method

---
+++ Java 8/src/com/defaultAndStatic/StudentDAO.java
@@ -0,0 +1,35 @@
+package com.defaultAndStatic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public interface StudentDAO 
+{
+
+	Map<String,String> studentDetails = new HashMap<String, String>();
+	public void getById();
+	public void fetchDetailsByCode();
+	public static void displayInterfaceName()
+	{
+		System.out.println("Studennt DAO Interface");
+	}
+	
+	public default void printAllDetails()
+	{  
+		for(Map.Entry<String,String> entry : studentDetails.entrySet())
+		{
+			System.out.println("Name is "+entry.getValue()+" and mobileNumber is "+entry.getKey());	
+		}
+	}
+	public default void setStudentNameByMobileNumber(String mobileNumber, String name)
+	{
+		studentDetails.put(mobileNumber,name);
+
+	}
+
+	public default String getStudentNameByAadharNumber(String mobileNumber)
+	{
+		return studentDetails.get(mobileNumber);
+	}
+}
---
+++ Java 8/src/com/defaultAndStatic/StudentDAOImpl.java
@@ -0,0 +1,42 @@
+package com.defaultAndStatic;
+
+import java.util.Map;
+
+public class StudentDAOImpl implements StudentDAO
+{
+
+	@Override
+	public void getById() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void fetchDetailsByCode() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* Override Method as per requirment */
+	public void printAllDetails() 
+	{  
+		for(Map.Entry<String,String> entry : studentDetails.entrySet())
+		{
+			System.out.println("Name is "+entry.getValue());	
+		}
+	}
+	public static void main(String[] args) 
+	{
+		StudentDAO dao=new StudentDAOImpl();
+		dao.setStudentNameByMobileNumber("9907820661","Ravikant Namdev");
+		dao.setStudentNameByMobileNumber("9098880661","Sumit Kushwah");
+		dao.setStudentNameByMobileNumber("887886735","Shiraj Saxena");
+		dao.setStudentNameByMobileNumber("9988820661","Kunal Namdev");
+		dao.setStudentNameByMobileNumber("90988861","kapil Kushwah");
+         
+		
+		dao.printAllDetails();
+	}
+	
+
+}
